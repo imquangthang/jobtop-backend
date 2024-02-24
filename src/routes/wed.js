a40e672ab34cd1a4ee7f803739e839cc82f07976
ACER <ACER@IMTHANG>
2024-02-24 20:53:09 +0700

    p5.4 - update user

--- src/routes/wed.js
+++ src/routes/wed.js
@@ -13,6 +13,8 @@ const initWedRoutes = (app) => {
   router.get("/user", homeControllor.handleUserPage);
   router.post("/users/create-user", homeControllor.handleCreateNewUser);
   router.post("/delete-user/:id", homeControllor.handleDeleteUser);
+  router.get("/update-user/:id", homeControllor.getUpdateUserPage);
+  router.post("/user/update-user", homeControllor.handleUpdateUser);
 
   return app.use("/", router);
 };
