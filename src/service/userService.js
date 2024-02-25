3a7e2086ffa01534f09f009f716eea4e2dabdab9
ACER <ACER@IMTHANG>
2024-02-25 08:53:38 +0700

    p6.2 - naming model

--- src/service/userService.js
+++ src/service/userService.js
@@ -20,7 +20,7 @@ const createNewUser = async (email, password, username) => {
 
   try {
     const [rows, fields] = await connection.execute(
-      "INSERT INTO users (email, password, username) VALUES (?,?,?)",
+      "INSERT INTO user (email, password, username) VALUES (?,?,?)",
       [email, hashPass, username]
     );
   } catch (error) {
@@ -37,7 +37,7 @@ const getUserList = async () => {
   });
 
   try {
-    const [rows, fields] = await connection.execute("Select * from users");
+    const [rows, fields] = await connection.execute("Select * from user");
     return rows;
   } catch (error) {
     console.log(">>> check error: ", error);
@@ -54,7 +54,7 @@ const deleteUser = async (id) => {
 
   try {
     const [rows, fields] = await connection.execute(
-      "DELETE FROM users WHERE id=?",
+      "DELETE FROM user WHERE id=?",
       [id]
     );
     return rows;
@@ -73,7 +73,7 @@ const getUserById = async (id) => {
 
   try {
     const [rows, fields] = await connection.execute(
-      "SELECT * FROM users WHERE id=?",
+      "SELECT * FROM user WHERE id=?",
       [id]
     );
     return rows;
@@ -92,7 +92,7 @@ const updateUserInfor = async (email, username, id) => {
 
   try {
     const [rows, fields] = await connection.execute(
-      "UPDATE users SET email = ?, username = ? WHERE id = ?",
+      "UPDATE user SET email = ?, username = ? WHERE id = ?",
       [email, username, id]
     );
     return rows;
