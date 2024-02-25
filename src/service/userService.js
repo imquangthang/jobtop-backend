0925cdc3c1dd8d9c90d693e3baacb331ff079b42
ACER <ACER@IMTHANG>
2024-02-25 09:48:16 +0700

    p7 - orm create user

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 import mysql from "mysql2/promise";
 import bluebird from "bluebird";
+import db from "../models/index";
 
 const salt = bcrypt.genSaltSync(10);
 
@@ -9,20 +10,13 @@ const hashUserPassword = (userPassword) => {
 };
 
 const createNewUser = async (email, password, username) => {
-  const connection = await mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    database: "jwt",
-    Promise: bluebird,
-  });
-
   let hashPass = hashUserPassword(password);
-
   try {
-    const [rows, fields] = await connection.execute(
-      "INSERT INTO user (email, password, username) VALUES (?,?,?)",
-      [email, hashPass, username]
-    );
+    await db.User.create({
+      username: username,
+      email: email,
+      password: hashPass,
+    });
   } catch (error) {
     console.log(">> check error: ", error);
   }
