3f5c836fea5c20d0f95eb5fa6d416a2b928d399c
ACER <ACER@IMTHANG>
2024-03-02 09:49:36 +0700

    P10.3.2 - API Login

--- src/service/loginRegisterService.js
+++ src/service/loginRegisterService.js
@@ -1,5 +1,6 @@
 import db from "../models/index";
 import bcrypt from "bcryptjs";
+import { Op } from "sequelize";
 
 const salt = bcrypt.genSaltSync(10);
 
@@ -71,6 +72,56 @@ const registerNewUser = async (rawUserData) => {
   }
 };
 
+const checkPassword = (inputPassword, hashPassword) => {
+  return bcrypt.compareSync(inputPassword, hashPassword); //true or false
+};
+
+const handleUserLogin = async (rawData) => {
+  try {
+    let user = await db.User.findOne({
+      where: {
+        [Op.or]: [{ email: rawData.valueLogin }, { phone: rawData.valueLogin }],
+      },
+    });
+
+    if (user) {
+      console.log(">>> Found user with email/phone");
+      let isCorrectPassword = await checkPassword(
+        rawData.password,
+        user.password
+      );
+
+      if (isCorrectPassword === true) {
+        return {
+          EM: "ok!",
+          EC: 0,
+          DT: "",
+        };
+      }
+    }
+
+    console.log(
+      ">>> input user with email/phone: ",
+      rawData.valueLogin,
+      "Password: ",
+      rawData.password
+    );
+
+    return {
+      EM: "Your email/phone or password is incorrect",
+      EC: 1,
+      DT: "",
+    };
+  } catch (error) {
+    console.log(">> check error: ", error);
+    return {
+      EM: "Something wrongs in service....",
+      EC: -2,
+    };
+  }
+};
+
 module.exports = {
   registerNewUser,
+  handleUserLogin,
 };
