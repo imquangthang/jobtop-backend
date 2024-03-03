65be122c7395ea2306c18e6d9233c6b04447f576
ACER <ACER@IMTHANG>
2024-03-03 19:23:19 +0700

    P11.2 - How To Use JWT

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import configCors from "./config/cors";
 require("dotenv").config();
 import bodyParser from "body-parser";
 // import connection from "./config/connectDB";
+import { createJWT, verifyToken } from "./middleware/JWTAction";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -22,6 +23,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // test connection DB
 // connection();
+
+// test jwt
+createJWT();
+let decoded = verifyToken(
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiVEhBTkciLCJhZGRyZXNzIjoiUVVBTkcgTkFNIiwiaWF0IjoxNzA5NDY4NDkzfQ.XCZVhygN_G8r1uSnlTV5Pd7UyaHQnHjZDrruNqorUmQ"
+);
+
+console.log(decoded);
 
 // init wed routes
 initWedRoutes(app);
