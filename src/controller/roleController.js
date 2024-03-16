a906814c9d5056637f86b59afd21947ca895560d
ACER <ACER@IMTHANG>
2024-03-16 19:39:57 +0700

    12.5 - Display/Delete Roles

--- src/controller/roleController.js
+++ src/controller/roleController.js
@@ -3,24 +3,33 @@ import roleApiService from "../service/roleApiService";
 
 const readFunc = async (req, res) => {
   try {
-    if (req.query.page && req.query.limit) {
-      let page = req.query.page;
-      let limit = req.query.limit;
+    // // use Paging
+    // if (req.query.page && req.query.limit) {
+    //   let page = req.query.page;
+    //   let limit = req.query.limit;
 
-      let data = await userApiService.getUserWithPagination(+page, +limit);
-      return res.status(200).json({
-        EM: data.EM, // error message
-        EC: data.EC, // error code
-        DT: data.DT, //data
-      });
-    } else {
-      let data = await userApiService.getAllUsers();
-      return res.status(200).json({
-        EM: data.EM, // error message
-        EC: data.EC, // error code
-        DT: data.DT, //data
-      });
-    }
+    //   let data = await userApiService.getUserWithPagination(+page, +limit);
+    //   return res.status(200).json({
+    //     EM: data.EM, // error message
+    //     EC: data.EC, // error code
+    //     DT: data.DT, //data
+    //   });
+    // } else {
+    //   let data = await userApiService.getAllUsers();
+    //   return res.status(200).json({
+    //     EM: data.EM, // error message
+    //     EC: data.EC, // error code
+    //     DT: data.DT, //data
+    //   });
+    // }
+
+    // not use Paging
+    let data = await roleApiService.getAllRoles();
+    return res.status(200).json({
+      EM: data.EM, // error message
+      EC: data.EC, // error code
+      DT: data.DT, //data
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).json({
@@ -69,7 +78,7 @@ const updateFunc = async (req, res) => {
 
 const deleteFunc = async (req, res) => {
   try {
-    let data = await userApiService.deleteUser(req.body.id);
+    let data = await roleApiService.deleteRole(req.body.id);
     return res.status(200).json({
       EM: data.EM, // error message
       EC: data.EC, // error code
